9bdaa8d734626c9eb6c516fd5f6ff59b3b169ebf
Khaykhaystar <kanyinsolakehinde81@gmail.com>
2023-12-16 23:46:37 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints using puts
+ *
+ * Return: Always 0(Success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\,\n")
+		return (0);
+}
