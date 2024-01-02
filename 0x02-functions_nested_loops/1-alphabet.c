ada427c05b930cf0e694eab778bf172a6e0573db
Khaykhaystar <kanyinsolakehinde81@gmail.com>
2024-01-02 21:12:07 +0100

    print lowercase letters with putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,14 @@
 
 
 /**
- * main - Entry point
+ * print_alphabet - print alphabet in lower case
  *
  * Return: Always 0(Success)
  */
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
