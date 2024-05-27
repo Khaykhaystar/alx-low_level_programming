55e707d99f164b9e00fec801cf776467e8342e0f
Khaykhaystar <kanyinsolakehinde81@gmail.com>
2024-05-27 22:30:19 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print values from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for(i='0'; i<='9'; i++)
+	
+	_putchar(i);
+	_putchar('\n');
+}
